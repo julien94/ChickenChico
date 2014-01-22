841888b74490e8f4ea28e06c9b9194d6480446c9
Julien <kyhoko@msn.com>
2014-01-22 17:06:00 +0100

    upd formu admin

--- vue/admin.php
+++ vue/admin.php
@@ -10,10 +10,17 @@
     </head>
     <body>
         <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
             ga('create', 'UA-46720288-1', 'chickenchico.com');
             ga('send', 'pageview');
@@ -28,12 +35,23 @@
                     <a href="/contact"><h4>Nous contacter</h4></a>
                 </div>
             </div>
-            <div id="liste">
-                <div class="formprod">
-                <h1>Ajouter un produit</h1>
-                <form>
-                    <div>Titre</div>
-                    <input type="text" class="addprod">
+            <div class="formpage">
+                <h1>CATEGORIES</h1>
+                <form class="add">
+                    <input type="text" name="">
+                    <input type="submit" value="Ajouter">
+                </form>   
+                <form class="upddel">
+                    <select name="categoryName">
+                        <option></option>
+                        // Affiche la liste des categories
+                    </select>
+                    <input type="submit" value="Modifier" name="updcat">
+                    <input type="submit" value="Supprimer" name="delcat">
+                </form> 
+                <hr>
+                <h1>PRODUITS</h1>
+                <form>    
                     <div>Description</div>
                     <div><textarea name="descri" class="addprod"></textarea></div>
                     <div>Prix Unité</div>
@@ -49,12 +67,15 @@
                         <option>dessert</option>
                     </select>
                 </form>
-                </div>
             </div>
         </div>
         <script>
-            $("#menu h4").mouseover(function(){$(this).css("box-shadow", "1px 1px 20px rgba(255, 255, 0, 0.5)");});
-            $("#menu h4").mouseout(function(){$(this).css("box-shadow", "0px 0px 15px rgba(0, 100, 255, 0.5)");});
+            $("#menu h4").mouseover(function() {
+                $(this).css("box-shadow", "1px 1px 20px rgba(255, 255, 0, 0.5)");
+            });
+            $("#menu h4").mouseout(function() {
+                $(this).css("box-shadow", "0px 0px 15px rgba(0, 100, 255, 0.5)");
+            });
         </script>
     </body>
 </html>
