498e83967f26c0e2f605ea7e60a3f35d3af96dde
julien94 <kyhoko@msn.com>
2014-02-08 00:17:18 +0100

    upd checkField

--- form/checkField.php
+++ form/checkField.php
@@ -42,21 +42,37 @@ class checkField extends controleur{
     }
     
     private function testDescription($data){
-        
+        if(count_chars($data) > 150){
+             $this->message = "La description est trop grande !!! Max : 150 caractère";
+             $this->render('admin');
+        }
     }
     
-    private function testPrix(){
-         if(preg_match("", $data)){
-                $this->message = "Un des champs contient un caractère non autorisé ! ex:()[]'\"\\<>";
+    private function testPrix($data){
+         if(!preg_match("!^[0-9]{1,2}([.][0-9]{0,2})?$!", $data)){
+                $this->message = "Un des montants est mal saisie ! attention a la syntaxe";
                 $this->render('admin');
             }
     }
     
-    private function testSelectField($data){
+    private function testSelect($data){
         
     }
     
-    private function testImageField($data){
-        
+    private function testImage(){
+        if($_FILES['image']['error'] > 0){
+            $this->message = "Erreur de chargement d'image !";
+            $this->render('admin');
+        }
+        $extend = explode('.', $_FILES['image']['name']);
+        if(strtolower($extend[1]) != jpg || strtolower($extend[1]) != png){
+            $this->message = "Extention d'image incorrect ! .jpg ou .png attendue !";
+            $this->render('admin');
+        }
+        if($_FILES['image']['size'] > 1024){
+            $this->message = "Erreur de chargement d'image !";
+            $this->render('admin');
+        }
+        if($_FILES['image']['tmp_name']){;}
     }
 }
